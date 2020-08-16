f3e2b320d1eb5145bd661a567fb3f515a2065e42
MrSpider <spideralpha8@gmail.com>
2020-08-16 04:11:55 -0400

    done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,126 +1,77 @@
 <template>
 <div>
   <b-container>
-    <b-row>
-      <b-col>
-          <b-card
-    no-body
+    <div class="row">
+      <div class="col-lg-3 col-md-4 col-sm-6 col-xs-1 mb-3" v-for="article in articles" :key="article">
+        <b-card
+        no-body
     style="max-width: 20rem;"
-    img-src="https://placekitten.com/380/200"
+    :img-src="articles.image"
     img-alt="Image"
     img-top
-  >
-    <template v-slot:header>
-      <h4 class="mb-0">Hello World</h4>
-    </template>
+    v-if="article"
+    >
+            <template v-slot:header>
+              <h4 class="mb-0">{{article.title}}</h4>
+            </template>
 
-    <b-card-body>
-      <b-card-text>
-        Some quick example text to build on the card title and make up the bulk of the card's
-        content.
-      </b-card-text>
-    </b-card-body>
+            <b-card-body>
+              <b-card-text>
+                {{article.description}}
+              </b-card-text>
+            </b-card-body>
 
-
-
-    <template v-slot:footer>
-        <small class="text-muted">Last updated 3 mins ago</small>
-    </template>
-  </b-card>
-      </b-col>
-
-      <b-col>
-          <b-card
-    no-body
-    style="max-width: 20rem;"
-    img-src="https://placekitten.com/380/200"
-    img-alt="Image"
-    img-top
-  >
-    <template v-slot:header>
-      <h4 class="mb-0">Hello World</h4>
-    </template>
-
-    <b-card-body>
-      <b-card-text>
-        Some quick example text to build on the card title and make up the bulk of the card's
-        content.
-      </b-card-text>
-    </b-card-body>
-
-
-
-    <template v-slot:footer>
-        <small class="text-muted">Last updated 3 mins ago</small>
-    </template>
-  </b-card>
-      </b-col>
-
-      <b-col>
-          <b-card
-    no-body
-    style="max-width: 20rem;"
-    img-src="https://placekitten.com/380/200"
-    img-alt="Image"
-    img-top
-  >
-    <template v-slot:header>
-      <h4 class="mb-0">Hello World</h4>
-    </template>
-
-    <b-card-body>
-      <b-card-text>
-        Some quick example text to build on the card title and make up the bulk of the card's
-        content.
-      </b-card-text>
-    </b-card-body>
-
-
-
-    <template v-slot:footer>
-        <small class="text-muted">Last updated 3 mins ago</small>
-    </template>
-  </b-card>
-      </b-col>
-
-      <b-col>
-          <b-card
-    no-body
-    style="max-width: 20rem;"
-    img-src="https://placekitten.com/380/200"
-    img-alt="Image"
-    img-top
-  >
-    <template v-slot:header>
-      <h4 class="mb-0">Hello World</h4>
-    </template>
-
-    <b-card-body>
-      <b-card-text>
-        Some quick example text to build on the card title and make up the bulk of the card's
-        content.
-      </b-card-text>
-    </b-card-body>
-
-
-
-    <template v-slot:footer>
-        <small class="text-muted">Last updated 3 mins ago</small>
-    </template>
-  </b-card>
-      </b-col>
-    </b-row>
+            <template v-slot:footer>
+                <small class="text-muted">{{article.date}}</small>
+            </template>
+        </b-card>
+      </div>
+    </div>
+    <div v-if="printError" class="alert alert-danger container" role="alert">
+              {{printError}}
+    </div>
   </b-container>
 </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'HelloWorld'
+  name: 'HelloWorld',
+
+  data(){
+    return{
+      articles: {
+        title: '',
+        description: '',
+        date: '',
+        image: ''
+      },
+      printError: "",
+    }
+  },
+
+  mounted(){
+    const apiKey = "d7216f8e81747dbef16cabf1b9ce825d";
+
+    axios.get(`https://gnews.io/api/v3/search?q=technology&token=${apiKey}`)
+      .then(response => {
+        this.articles = response.data.articles;
+        this.articles.title = response.data.articles.title;
+        this.articles.description = response.data.articles.description;
+        this.articles.date = response.data.articles.publishedAt;
+        this.articles.image = response.data.articles.image;
+        console.log(response.data.articles);
+      })
+      .catch(error => {
+        console.log(error);
+        error = "Impossible d'afficher les articles, Nombres de requêtes atteintes";
+        this.printError = error;
+      })
+  }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
 </style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import axios from 'axios'
 
+Vue.prototype.$http = axios
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.config.productionTip = false
